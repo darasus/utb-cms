fb5c4b85c2f31dd5fc6c163d4733f4fb617301dd
Ilya Daraseliya <idarase@gmail.com>
2020-06-02 21:39:21 +0200

    fix

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -92,8 +92,9 @@ $table_prefix = 'wp_';
 define( 'WP_DEBUG', false );
 
 /* Multisite */
+define('WP_ALLOW_MULTISITE', true);
 define('MULTISITE', true);
-define('SUBDOMAIN_INSTALL', true);
+define('SUBDOMAIN_INSTALL', false);
 define('DOMAIN_CURRENT_SITE', 'nippel.systems');
 define('PATH_CURRENT_SITE', '/');
 define('SITE_ID_CURRENT_SITE', 1);
